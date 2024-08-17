b3042b862d360477ad5ced5e1b7c9587db0c4e8c
ali haitham <ali.haitham87@outlook.com>
2024-08-17 16:37:10 +0300

    [TT]: default date of OPM

--- aesp_overtime_hr/models/hr_overtime_per_month.py
+++ aesp_overtime_hr/models/hr_overtime_per_month.py
@@ -2,13 +2,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
-
+from datetime import datetime, timedelta
+import calendar
 
 class HrOvertimePerMonth(models.Model):
     _name = 'hr.overtime.per.month'
 
-    from_date = fields.Date(string='Period Start Date')
-    to_date = fields.Date(string='Period End Date')
+    def _get_last_day_of_current_month(self):
+        today = datetime.today()
+        range = calendar.monthrange(today.year, today.month)
+        first_day = today.replace(day=range[1])
+        return first_day.date()
+
+    def _get_first_day_of_current_month(self):
+        today = datetime.today()
+        range = calendar.monthrange(today.year, today.month)
+        first_day = today.replace(day=range[0])
+        return first_day.date()
+
+    from_date = fields.Date(string='Period Start Date', default=lambda self: self._get_first_day_of_current_month())
+    to_date = fields.Date(string='Period End Date', default=lambda self: self._get_last_day_of_current_month())
     overtime_hours = fields.Float(string='Working Hours')
     employee_id = fields.Many2one('hr.employee', string='Employee')
 
