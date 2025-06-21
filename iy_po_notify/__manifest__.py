fe2ea77add76d46d4ced4c51c82ced24dd8278a8
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-06-21 14:20:06 +0300

    Update __manifest__.py

--- iy_po_notify/__manifest__.py
+++ iy_po_notify/__manifest__.py
@@ -9,6 +9,7 @@
     'author': 'Iyad Husary',
     'depends': ['purchase'],
     'data': [
+        'security/ir.model.access.csv',
         'views/notification_config_views.xml',
         'views/email_templates.xml',
     ],
