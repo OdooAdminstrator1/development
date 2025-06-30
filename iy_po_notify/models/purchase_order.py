3704d915c043591c8a13c06ff6494c0f79b892a9
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-06-30 20:26:34 +0300

    Update purchase_order.py

--- iy_po_notify/models/purchase_order.py
+++ iy_po_notify/models/purchase_order.py
@@ -15,7 +15,7 @@ class PurchaseOrder(models.Model):
         if not config or not config.user_ids:
             return
             
-        template = self.env.ref('purchase.po_approval_notification_email_tem', False)
+        template = self.env.ref('iy_po_notify.po_approval_notification_email_tem', False)
         if not template:
             raise UserError(_("Email template not found"))
             
