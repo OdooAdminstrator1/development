33cd19e3692789ef886506b6a28f94387475c316
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-07-05 19:11:13 +0300

    Add files via upload

---
+++ iy_po_notify/models/stock_immediate_Trans.py
@@ -0,0 +1,59 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class StockImmediateTransferEmail(models.TransientModel):
+    _inherit = 'stock.immediate.transfer'
+    
+
+    def process(self):
+
+        res = super(StockImmediateTransferEmail, self).process()
+        if res:
+           for line in self.immediate_transfer_line_ids:
+                if (line.to_immediate is True) and line.state == 'assigned':
+                        self.send_validation_notification(self,line)
+
+        return res
+    
+    def send_validation_notification(self,picking):
+        config = self.env['purchase.notification.config'].search([], limit=1)
+        if not config or not config.user_ids:
+            return
+            
+            
+        for user in config.user_ids:
+            self.send_custom_email(user,picking)
+
+    
+    def send_custom_email(self,userr,picking):#partner_id
+        str_body=f"""
+            <div>
+                <p>Dear {userr.name} ,</p>
+                <p>The following inventory receipt has been validated by {self.env.user.name}:</p>
+                <p>Inventory Reference: {picking.name}</p>
+                <p>Vendor: {picking.partner_id.name}</p>
+                <p>Receipt Date: {picking.date_done}</p>
+                <p>You can view the receipt here: 
+                    <a href='/web#id={picking.id}&amp;model=stock.picking&amp;view_type=form' >
+                        View Inventory Receipt
+                    </a>
+                </p>
+            </div>
+        """
+
+        mail_values = {
+            'subject': 'Receipt is confirmed',
+            'body_html': str_body,
+            'email_from': self.env.user.email,
+            'email_to': userr.email,
+            'model': 'stock.picking',
+            'res_id': picking.id,
+        }
+        # Create and send email
+        mail = self.env['mail.mail'].sudo().create(mail_values)
+        mail.send()
+
+
+
+    
