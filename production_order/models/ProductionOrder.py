c3b94fa9d97823c3d01316312c3a7289c3122c1e
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-08-14 08:35:08 +0300

    Add files via upload

---
+++ production_order/models/ProductionOrder.py
@@ -0,0 +1,44 @@
+from odoo import api, fields, models, tools
+
+class ProductionOrder(models.Model):
+    _name = 'production.order'
+    _description = 'Production Order'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    opportunity_id = fields.Many2one('crm.lead', string='Opportunity', required=True)
+    partner_id = fields.Many2one('res.partner', string='Customer', compute='_compute_partner', store=True, readonly=True)
+
+    @api.depends('opportunity_id')
+    def _compute_partner(self):
+        for record in self:
+            record.partner_id = record.opportunity_id.partner_id
+
+
+    @api.onchange('opportunity_id')
+    def _onchange_opportunity_id(self):
+        """Auto-fill name from opportunity if empty."""
+        if self.opportunity_id and not self.name:
+            self.name = self.opportunity_id.name
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    production_order_id = fields.Many2one(
+        'production.order',
+        string='Production Order'
+       # ,domain="[('partner_id', '=', partner_id)]"
+    )
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+   # partner_id = fields.Many2one('res.partner', string='Customer')
+    production_order_id = fields.Many2one(
+        'production.order',
+        string='Production Order'
+        #,domain="[('partner_id', '=', partner_id)]"
+    )
+
+
+            # otherwise keep partner_id as-is (user can fill it manually)
