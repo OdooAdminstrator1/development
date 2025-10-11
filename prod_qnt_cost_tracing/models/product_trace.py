800ad54685cfa1be68e96712f203ecc7627aff87
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-10-11 15:47:48 +0300

    Update product_trace.py

--- prod_qnt_cost_tracing/models/product_trace.py
+++ prod_qnt_cost_tracing/models/product_trace.py
@@ -43,59 +43,161 @@ class ProductTrace(models.Model):
     ], string='Move Type', required=False)
 
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
-
+    
+    
     # Helper method for creation from valuation layer
     @api.model
-    def create_from_valuation_layer(self, valuation_layer):
+    def create_from_valuation_layer(self, valuation_layer,trace_rescords,isHook):
         """Create a product trace record from a stock_valuation_layer record."""
-     #   move_type = self._get_move_type_from_layer(valuation_layer)
-        max_id_record = self.env['stock.product.trace'].search([], order='id desc', limit=1)
-        # rec =self.env['stock.product.trace'].browse(max_id_record)
-        # old_c=0
-        # old_q=0
-
-        # if (rec):
-        #     old_c= rec.qty_new
-        #     old_q= rec.cost_new_value
 
 
-        return self.create({
+        newtrace= self.create({
             'date': valuation_layer.create_date,
             'reference': valuation_layer.description,
             'product_id': valuation_layer.product_id.id,
             'cost_unit_value': valuation_layer.unit_cost,
-            # 'cost_old_value': old_c, #valuation_layer.value - valuation_layer.remaining_value,
-            # 'qty_old' : old_q,
             'cost_new_value': valuation_layer.value,
             'qty_done': valuation_layer.quantity,
             'move_id': valuation_layer.account_move_id.id,
             'ref_value': valuation_layer.description or valuation_layer.stock_move_id.name,
-          #  'stock_move_type': move_type,
         })
+        # if  isHook:
+        #     trace_rescords+=newtrace
+        # else:
+        #     trace_rescords=None
+        newtrace.post_init_hook(valuation_layer,trace_rescords,isHook)
 
-    def _get_move_type_from_layer(self, valuation_layer):
-        """Deduce the move type based on related model fields."""
-        move = valuation_layer.stock_move_id
-        if not move:
-            if valuation_layer.stock_landed_cost_id:
-                return 'landed_cost'
-            return 'cost_manually'
+        return newtrace
 
-        picking = move.picking_id
-        if picking:
-            if picking.picking_type_id.code == 'incoming':
-                return 'preceipt'
-            elif picking.picking_type_id.code == 'outgoing':
-                return 'sdeliver'
-            elif 'return' in (picking.name or '').lower():
-                return 'sreturn'
-        if move.raw_material_production_id:
-            return 'man_deliver'
-        if move.production_id:
-            return 'man_unbuilt'
-        if move.scrapped:
-            return 'scrap'
-        return 'adjustment'
+
+    def post_init_hook(self,valuation_layers,trace_rescords,isHook):
+        """Backfill product trace records for existing valuation layers."""
+        env = self.env
+        new_trace=self
+
+
+        for vl in valuation_layers:
+                # Skip if trace already exists for this valuation layer
+        # if trace_model.search_count([('move_id', '=', vl.account_move_id.id)]):
+            #    continue
+        #   if not vl.account_move_id.id:
+        #       continue
+
+            product_id = vl.product_id.id
+            loc_id=vl.stock_move_id.location_id
+            loc_dest_id=vl.stock_move_id.location_dest_id
+            stock_landed_cost_id =vl.stock_landed_cost_id
+            is_finished=False
+            is_unbuild=vl.stock_move_id.unbuild_id
+            loc_dest_usage=vl.stock_move_id.location_dest_id.usage
+            is_finished= vl.stock_move_id.production_id
+            is_component=not (is_unbuild and product_id==is_unbuild.product_id.id)
+
+            #  Get the latest trace record for the same product
+
+            if isHook:
+                # last_trace=trace_rescords
+                # valuation_layers = env['stock.product.trace'].search([], order='id')
+                max_layer = False
+                max_id = 0
+                if trace_rescords:
+                    for layer in trace_rescords:
+                        if layer.product_id.id == product_id and layer.id > max_id:
+                            max_id = layer.id
+                            max_layer = layer
+
+                last_trace = max_layer
+            else:
+                trace_model = env['stock.product.trace'].sudo()
+                last_trace = trace_model.search(
+                    [('product_id', '=', product_id)],
+                    order='id desc',
+                    limit=1
+                )
+
+            #  Determine old cost value
+            cost_old_value = last_trace.cost_new_value if last_trace else 0.0
+            qty_old_value = last_trace.qty_new if last_trace else 0.0
+
+            #  Create the trace from valuation layer
+            #new_trace = trace_model.create_from_valuation_layer(vl)
+
+            #  Update the old cost value
+        # new_trace.cost_old_value = cost_old_value
+            new_trace.cost_old_value=cost_old_value
+            new_trace.qty_new = qty_old_value+new_trace.qty_done
+            new_trace.qty_old = qty_old_value
+
+            new_trace.cost_new_value=cost_old_value if last_trace else vl.unit_cost
+        # new_avg_cost=(cost_old_value*qty_old_value+new_trace.qty_done*vl.unit_cost)/(qty_old_value+new_trace.qty_done)
+
+            if stock_landed_cost_id:
+                new_trace.stock_move_type='landed_cost'
+                if qty_old_value:
+                    new_trace.cost_new_value=(vl.value+cost_old_value*qty_old_value)/qty_old_value
+                else:
+                    new_trace.cost_new_value=new_trace.cost_old_value
+
+            elif loc_id.name=='Vendors':
+                new_trace.stock_move_type='preceipt'
+                if not (qty_old_value+new_trace.qty_done):
+                    new_trace.cost_new_value=new_trace.cost_old_value
+                else:
+                    new_trace.cost_new_value=(cost_old_value*qty_old_value+new_trace.qty_done*vl.unit_cost)/(qty_old_value+new_trace.qty_done)
+            
+            elif loc_id.name=='Customers':
+                new_trace.stock_move_type='sreturn'
+                if (qty_old_value+new_trace.qty_done):
+                    new_trace.cost_new_value=(cost_old_value*qty_old_value+new_trace.qty_done*vl.unit_cost)/(qty_old_value+new_trace.qty_done)
+                else:
+                    new_trace.cost_new_value=new_trace.cost_old_value
+
+            elif loc_dest_id.name=='Vendors':
+                new_trace.stock_move_type='preturn'
+
+            elif loc_dest_id.name=='Customers':
+                new_trace.stock_move_type='sdeliver'
+
+            elif loc_id.name=='Inventory adjustment' or loc_dest_id.name=='Inventory adjustment':
+                new_trace.stock_move_type='adjustment'
+
+            elif (not loc_id.name) and  (not loc_dest_id):
+                new_trace.stock_move_type='cost_manually'
+                if  qty_old_value:
+                    new_trace.cost_new_value=cost_old_value+vl.value/qty_old_value
+                else:
+                    new_trace.cost_new_value=new_trace.cost_old_value
+                
+            elif (loc_dest_id.name=='Scrap'):
+                new_trace.stock_move_type='scrap'
+            
+            elif (loc_dest_usage=='inventory'):
+                new_trace.stock_move_type='inventory_loss'
+            
+            elif (is_finished and loc_id.name=='Production'):
+                new_trace.stock_move_type='manufacturing'
+                if (qty_old_value+new_trace.qty_done):
+                    new_trace.cost_new_value=(cost_old_value*qty_old_value+new_trace.qty_done*vl.unit_cost)/(qty_old_value+new_trace.qty_done)
+                else:
+                    new_trace.cost_new_value=new_trace.cost_old_value
+                
+            elif (is_unbuild and product_id==is_unbuild.product_id.id and loc_dest_id.name=='Production'):
+                new_trace.stock_move_type='unbuilt'
+            
+            elif (is_component and loc_id.name=='Production'):
+                if not (qty_old_value+new_trace.qty_done):
+                    new_trace.cost_new_value=new_trace.cost_old_value
+                else:
+                    new_trace.cost_new_value=(cost_old_value*qty_old_value+new_trace.qty_done*vl.unit_cost)/(qty_old_value+new_trace.qty_done)
+                new_trace.stock_move_type='unbuilt_raw'
+            
+            elif (is_component and loc_dest_id.name=='Production'):
+                new_trace.stock_move_type='manufacturing_raw'
+            
+            else:
+                new_trace.stock_move_type='undefined'
+
+
 
 
 class StockValuationLayer(models.Model):
@@ -107,7 +209,8 @@ class StockValuationLayer(models.Model):
         trace_model = self.env['stock.product.trace'].sudo()
         for rec in records:
             try:
-                trace_model.create_from_valuation_layer(rec)
+                newtrace=trace_model.create_from_valuation_layer(rec,[],False)
+                newtrace.post_init_hook(rec)
                
             except Exception as e:
                 # You can log the error, but avoid breaking main flow
@@ -122,5 +225,6 @@ class StockValuationLayer(models.Model):
                     'line': 'create_hook',
                     'func': 'create_from_valuation_layer'
                 })
+        
         return records
 
