da3fdd3ed6189834f8fd3bcb834a9176b98f7f4d
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-10-11 16:57:10 +0300

    Update hooks.py

--- prod_qnt_cost_tracing/hooks.py
+++ prod_qnt_cost_tracing/hooks.py
@@ -3,12 +3,17 @@ from odoo import api, SUPERUSER_ID
 def post_init_hook(cr, registry):
     """Backfill product trace records for existing valuation layers."""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    valuation_layers = env['stock.valuation.layer'].search([], order='id')
+    valuation_layers = env['stock.valuation.layer'].search([], order='product_id, id')
     trace_model = env['stock.product.trace'].sudo()
     tracerecords =None
+    current_product=None
     for vl in valuation_layers:
         if tracerecords:
-            tracerecords=tracerecords+ trace_model.create_from_valuation_layer(vl,tracerecords,True)
+            if current_product and current_product==vl.product_id.id:
+                tracerecords=trace_model.create_from_valuation_layer(vl,tracerecords,True)
+            else:
+                tracerecords=trace_model.create_from_valuation_layer(vl,None,True)
+                current_product=vl.product_id.id
         else:
-            tracerecords=trace_model.create_from_valuation_layer(vl,tracerecords,True)
-        
+            tracerecords=trace_model.create_from_valuation_layer(vl,None,True)
+            current_product=vl.product_id.id
