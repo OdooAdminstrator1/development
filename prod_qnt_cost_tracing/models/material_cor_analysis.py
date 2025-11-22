a1724983a7679d81d50a7eb00b84a83569a4ffd8
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2025-11-22 16:41:57 +0300

    Update material_cor_analysis.py

--- prod_qnt_cost_tracing/models/material_cor_analysis.py
+++ prod_qnt_cost_tracing/models/material_cor_analysis.py
@@ -63,8 +63,9 @@ class MaterialCorAnalysis(models.Model):
         self._cr.execute("""
             CREATE OR REPLACE VIEW %s AS (
 select main.*,product.expense_account_id,product.income_account_id
-,RV.tot * main.revenue/ sum(main.revenue) OVER (PARTITION BY product.income_account_id) as rest_revenue
-,RC.tot,RC.tot*(main.cost)/sum(main.cost) over (PARTITION BY product.expense_account_id)  as rest_cost
+,RV.tot * main.revenue/ COALESCE(NULLIF(sum(main.revenue) OVER (PARTITION BY product.income_account_id), 0), 1)  as rest_revenue
+,RC.tot,RC.tot*(main.cost)/COALESCE(NULLIF(sum(main.cost) over (PARTITION BY product.expense_account_id), 0), 1)   as rest_cost
+
 from
 (
     select  COALESCE(g1.id, g2.product_id+1) as id,COALESCE(g1.product_id, g2.product_id) as product_id,COALESCE(g1.currency_id, g2.currency_id) as currency_id,g1.quantity,
