fba17b85439c48c86f1e32acf596be94ffe57913
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-01-29 19:07:02 +0300

    Add files via upload

--- prod_qnt_cost_tracing/models/clearance_manual_stockin.py
+++ prod_qnt_cost_tracing/models/clearance_manual_stockin.py
@@ -8,16 +8,20 @@ class ClearanceManualStockin(models.Model):
     move_id = fields.Many2one('account.move', 'Journal Entry', readonly=True)
     balance = fields.Float('Balance', readonly=True)
     date = fields.Date(string='Date', related='move_id.date',readonly=True,)
-
+    stock_move_type = fields.Selection([
+        ('qty_manualy', 'Update Quantity Manually'),
+        ('adjustment', 'Inventory Adjustment'),
+        ('cost_manually', 'Update Cost Manually'),
+    ], string='Move Type',readonly=True)
 
     def init(self):
         """Initialize SQL view"""
         tools.drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""
             CREATE OR REPLACE VIEW %s AS (
-select row_number() OVER(order by B.move_id) AS id, B.move_id as move_id,sum(B.balance) as balance from stock_product_trace A   inner join 
+select row_number() OVER(order by B.move_id) AS id, B.move_id as move_id,A.stock_move_type as stock_move_type,sum(B.balance) as balance from stock_product_trace A   inner join 
 account_move_line B on A.move_id=B.move_id
- where A.stock_move_type='cost_manually'
+ where A.stock_move_type in ('cost_manually','qty_manualy','adjustment')
  and B.parent_state='posted'
  and B.account_id=any(
                     string_to_array(
@@ -29,7 +33,7 @@ account_move_line B on A.move_id=B.move_id
                     )::int[]
                 )
 and B.date >(SELECT value FROM ir_config_parameter WHERE key = 'prod_qnt_cost_tracing.last_closing_year')::date 
-group by B.move_id
+group by B.move_id,A.stock_move_type
         )
         """ % self._table)
 
@@ -47,7 +51,7 @@ group by B.move_id
         sqql="""
 select COALESCE(sum(B.balance),0) from stock_product_trace A   inner join 
 account_move_line B on A.move_id=B.move_id
- where A.stock_move_type='cost_manually'
+ where A.stock_move_type in ('cost_manually','qty_manualy','adjustment')
  and B.parent_state='posted'
  and B.account_id=any(
                     string_to_array(
