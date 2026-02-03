fa03167e29587742b2302b5a0b4524afc95eddc4
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-02-03 17:50:38 +0300

    Update clearance_manual_journal_stockin.py

--- prod_qnt_cost_tracing/models/clearance_manual_journal_stockin.py
+++ prod_qnt_cost_tracing/models/clearance_manual_journal_stockin.py
@@ -14,14 +14,13 @@ class ClearanceManualJournalStockin(models.Model):
         tools.drop_view_if_exists(self._cr, self._table)
         self._cr.execute("""
             CREATE OR REPLACE VIEW %s AS (
-select row_number() OVER(order by A.id) AS id, A.id as move_id ,sum(ml.balance) as balance from
-(select am.id,am.date,am.name from account_move as am where am.stock_move_id is null) as A left join 
-(select distinct accl.move_id from account_move_line accl 
-where  accl.expense_id is not null or accl.statement_id is not null or accl.exclude_from_invoice_tab is not null or is_landed_costs_line=true) as B
-on A.id=B.move_id 
-inner join account_move_line ml on A.id =ml.move_id
-where B.move_id is null
- and ml.account_id=any(
+select  row_number() OVER(order by A.id) AS id, A.id as move_id ,sum(ml.balance) as balance from account_move as A
+inner join
+(select distinct move_id from account_move_line where name is null or length(name)<3) as B
+on A.id =B.move_id
+inner join account_move_line as ml on a.id= ml.move_id
+where A.move_type='entry' and A.state='posted' and A.stock_move_id is null
+and ml.account_id=any(
                     string_to_array(
                         replace(replace(
                             (SELECT value FROM ir_config_parameter WHERE key = 'prod_qnt_cost_tracing.clearance_ids'),
@@ -31,7 +30,7 @@ where B.move_id is null
                     )::int[]
                 )
 and A.date >(SELECT value FROM ir_config_parameter WHERE key = 'prod_qnt_cost_tracing.last_closing_year')::date 
-group by  A.id,A.date
+group by A.id
         )
         """ % self._table)
 
@@ -47,14 +46,13 @@ group by  A.id,A.date
 
     def getSum(self):
         sqql="""
-select COALESCE(sum(ml.balance),0) from
-(select am.id,am.date,am.name from account_move as am where am.stock_move_id is null) as A left join 
-(select distinct accl.move_id from account_move_line accl 
-where  accl.expense_id is not null or accl.statement_id is not null or accl.exclude_from_invoice_tab is not null or is_landed_costs_line=true) as B
-on A.id=B.move_id 
-inner join account_move_line ml on A.id =ml.move_id
-where B.move_id is null
- and ml.account_id=any(
+select COALESCE(sum(ml.balance),0) from account_move as A
+inner join
+(select distinct move_id from account_move_line where name is null or length(name)<3) as B
+on A.id =B.move_id
+inner join account_move_line as ml on a.id= ml.move_id
+where A.move_type='entry' and A.state='posted' and A.stock_move_id is null
+and ml.account_id=any(
                     string_to_array(
                         replace(replace(
                             (SELECT value FROM ir_config_parameter WHERE key = 'prod_qnt_cost_tracing.clearance_ids'),
