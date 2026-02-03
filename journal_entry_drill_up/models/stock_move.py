87a56ce8d941bf9f9999f5e0cb28021a310cbd9e
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-02-03 16:13:02 +0300

    Update stock_move.py

--- journal_entry_drill_up/models/stock_move.py
+++ journal_entry_drill_up/models/stock_move.py
@@ -69,6 +69,8 @@ class AccountMove(models.Model):
             else:
                 if record.move_type!='entry':
                     manual = False
+                elif record.record.stock_move_id:
+                    record.is_manual=False
                 if record.move_type=='entry':
                     for line in record.line_ids:
                         if line.name:
