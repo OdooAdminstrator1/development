7515292461f18323a3143efa66b164880ef27786
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-03-13 23:55:46 +0300

    Update product_trace.py

--- prod_qnt_cost_tracing/models/product_trace.py
+++ prod_qnt_cost_tracing/models/product_trace.py
@@ -187,8 +187,8 @@ and vl.account_move_id is not null;
     
     def getSummary(self,domain):
         records = self.env['stock.product.trace'].search(domain)
-        total = sum( record.result_value 
-        for record in records if record.result_value 
+        total = sum( record.newsubcost 
+        for record in records if record.newsubcost 
     )
         return format(int(total or 0),',') 
 
