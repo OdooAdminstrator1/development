d9abc9ec66e4843c42a342e8ef25cfafc23d452a
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-04-22 13:22:55 +0300

    Delete ledlight_invoice_with_qrcode directory

--- ledlight_invoice_with_qrcode/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import qrcode_gen
-from . import ledlight_invoice
-from . import hr_employee_inherited
-from . import res_config_setting
-from . import invoice_inheritred
