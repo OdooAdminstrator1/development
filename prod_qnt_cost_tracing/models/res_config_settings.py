8e56a6264d5449f9fbe2ea0b5ca4e14056eeab6c
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-04-26 13:07:34 +0300

    Update res_config_settings.py

--- prod_qnt_cost_tracing/models/res_config_settings.py
+++ prod_qnt_cost_tracing/models/res_config_settings.py
@@ -1,79 +1,106 @@
+# models/res_config_settings.py
+
 from odoo import models, fields, api
 from ast import literal_eval
+from odoo.exceptions import UserError
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    # 1. REMOVE config_parameter from all Many2many fields
     cost_account_ids = fields.Many2many(
         'account.account',
-        'res_config_trace_conf_rel',
-        'config_cost_id', 'account_cost_id',
-        string="Cost of Revenue Accounts"
+        'res_config_settingstrace_conf_rel',  # relation table
+        'config_cost_id', 'accout_cost_id',
+        string="Cost of Revenue Accounts",
+        # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     )
     cost_journal_ids = fields.Many2many(
         'account.journal',
-        'res_config_trace_cost_journa_rel',
+        'res_config_settingstrace_cost_journa_rel',  # relation table
         'config_id', 'journal_id',
-        string="Sales Journal"
+        string="Sales Journal", # Stock Valuation Journal, Revenue Acounts
+        # inventory valuation journal
     )
     revenue_ids = fields.Many2many(
         'account.account',
-        'res_config_trace_revenue_rel',
-        'config_rev_id', 'account_rev_id',
-        string="Revenue Accounts"
+        'res_config_settingstrace_revenue_rel',  # relation table
+        'config_rev_id', 'accout_rev_id',
+        string="Revenue Acounts", # Stock Valuation Journal, Revenue Acounts
+        # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
     inventory_val_journal_ids = fields.Many2many(
         'account.journal',
-        'res_config_trace_inval_journal_rel',
+        'res_config_settingstrace_inval_journal_rel',  # relation table
         'config_id', 'journal_id',
-        string="Inventory Valuation Journal"
+        string="Inventory Valuation Journal", # Stock Valuation Journal, Revenue Acounts
+        # inventory valuation journal
     ) 
     clearance_ids = fields.Many2many(
         'account.account',
-        'res_config_trace_clearance_acc_rel',
-        'config_clearance_id', 'account_clearance_id',
-        string="Clearance In Accounts"
+        'res_config_settingclearance_acc_rel',  # relation table
+        'config_clearance_id', 'accout_clearance_id',
+        string="clearance In Acounts", # Stock Valuation Journal, Revenue Acounts
+        # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
     clearance_out_ids = fields.Many2many(
         'account.account',
-        'res_config_trace_clearance_acc_out_rel',
-        'config_clearance_out_id', 'account_clearance_out_id',
-        string="Clearance Out Accounts"
+        'res_config_settingclearance_acc_ouy_rel',  # relation table
+        'config_clearance_out_id', 'accout_clearance_out_id',
+        string="clearance Out Acounts", # Stock Valuation Journal, Revenue Acounts
+        # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
-    
-    # Dates/Booleans/Chars can keep config_parameter safely
-    last_closing_year = fields.Date(
-        string="Last Closing Date",
-        config_parameter='prod_qnt_cost_tracing.last_closing_year'
-    )
+    last_closing_year=fields.Date(string="Last Closing Date")
 
-    # 2. MANUALLY load the values
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
-        get_param = self.env['ir.config_parameter'].sudo().get_param
-        
-        # We store M2M IDs as a string like "[1, 2, 3]" and convert back to list
+        cost_account_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.cost_account_ids')
+        cost_journal_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.cost_journal_ids')
+        revenue_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.revenue_ids')
+        inventory_val_journal_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.inventory_val_journal_ids')
+        clearance_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.clearance_ids')
+        clearance_out_ids = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.clearance_out_ids')
+        last_closing_year = self.env['ir.config_parameter'].sudo().get_param('prod_qnt_cost_tracing.last_closing_year')
+    
+        cra = False
+        rj=False
+        ra=False
+        ivj=False
+        clids=False
+        cloutids=False
+        lastCY=last_closing_year
+
+        if cost_account_ids:
+            cra = [(6, 0, literal_eval(cost_account_ids))]
+        if cost_journal_ids:
+            rj = [(6, 0, literal_eval(cost_journal_ids))]
+        if revenue_ids:
+            ra = [(6, 0, literal_eval(revenue_ids))]
+        if inventory_val_journal_ids:
+            ivj = [(6, 0, literal_eval(inventory_val_journal_ids))]
+        if clearance_ids:
+            clids = [(6, 0, literal_eval(clearance_ids))]
+        if clearance_out_ids:
+            cloutids = [(6, 0, literal_eval(clearance_out_ids))]
+
         res.update(
-            cost_account_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.cost_account_ids', '[]')))],
-            cost_journal_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.cost_journal_ids', '[]')))],
-            revenue_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.revenue_ids', '[]')))],
-            inventory_val_journal_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.inventory_val_journal_ids', '[]')))],
-            clearance_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.clearance_ids', '[]')))],
-            clearance_out_ids=[(6, 0, literal_eval(get_param('prod_qnt_cost_tracing.clearance_out_ids', '[]')))],
+            cost_account_ids=cra,
+            cost_journal_ids=rj,
+            revenue_ids= ra,
+            inventory_val_journal_ids= ivj,
+            clearance_ids=clids,
+            clearance_out_ids=cloutids,
+            last_closing_year=lastCY,
         )
         return res
 
-    # 3. MANUALLY save the values
     def set_values(self):
-        super(ResConfigSettings, self).set_values()
-        set_param = self.env['ir.config_parameter'].sudo().set_param
-        
-        # Save the list of IDs as a string representation
-        set_param('prod_qnt_cost_tracing.cost_account_ids', self.cost_account_ids.ids)
-        set_param('prod_qnt_cost_tracing.cost_journal_ids', self.cost_journal_ids.ids)
-        set_param('prod_qnt_cost_tracing.revenue_ids', self.revenue_ids.ids)
-        set_param('prod_qnt_cost_tracing.inventory_val_journal_ids', self.inventory_val_journal_ids.ids)
-        set_param('prod_qnt_cost_tracing.clearance_ids', self.clearance_ids.ids)
-        set_param('prod_qnt_cost_tracing.clearance_out_ids', self.clearance_out_ids.ids)
+        res = super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.cost_account_ids', self.cost_account_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.cost_journal_ids', self.cost_journal_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.revenue_ids', self.revenue_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.inventory_val_journal_ids', self.inventory_val_journal_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.clearance_ids', self.clearance_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.clearance_out_ids', self.clearance_out_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('prod_qnt_cost_tracing.last_closing_year', self.last_closing_year)
+        return res
