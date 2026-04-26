170a0bd6ed51de5808374b6cab496884872074ae
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-04-26 10:39:13 +0300

    Update res_config_settings.py

--- prod_qnt_cost_tracing/models/res_config_settings.py
+++ prod_qnt_cost_tracing/models/res_config_settings.py
@@ -12,6 +12,7 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingstrace_conf_rel',  # relation table
         'config_cost_id', 'accout_cost_id',
         string="Cost of Revenue Accounts",
+        config_parameter='prod_qnt_cost_tracing.cost_account_ids',
         # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     )
     cost_journal_ids = fields.Many2many(
@@ -19,6 +20,7 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingstrace_cost_journa_rel',  # relation table
         'config_id', 'journal_id',
         string="Sales Journal", # Stock Valuation Journal, Revenue Acounts
+        config_parameter='prod_qnt_cost_tracing.cost_journal_ids',
         # inventory valuation journal
     )
     revenue_ids = fields.Many2many(
@@ -26,6 +28,7 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingstrace_revenue_rel',  # relation table
         'config_rev_id', 'accout_rev_id',
         string="Revenue Acounts", # Stock Valuation Journal, Revenue Acounts
+        config_parameter='prod_qnt_cost_tracing.revenue_ids',
         # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
     inventory_val_journal_ids = fields.Many2many(
@@ -33,6 +36,7 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingstrace_inval_journal_rel',  # relation table
         'config_id', 'journal_id',
         string="Inventory Valuation Journal", # Stock Valuation Journal, Revenue Acounts
+        config_parameter='prod_qnt_cost_tracing.inventory_val_journal_ids',
         # inventory valuation journal
     ) 
     clearance_ids = fields.Many2many(
@@ -40,6 +44,7 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingclearance_acc_rel',  # relation table
         'config_clearance_id', 'accout_clearance_id',
         string="clearance In Acounts", # Stock Valuation Journal, Revenue Acounts
+        config_parameter='prod_qnt_cost_tracing.clearance_ids',
         # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
     clearance_out_ids = fields.Many2many(
@@ -47,9 +52,12 @@ class ResConfigSettings(models.TransientModel):
         'res_config_settingclearance_acc_ouy_rel',  # relation table
         'config_clearance_out_id', 'accout_clearance_out_id',
         string="clearance Out Acounts", # Stock Valuation Journal, Revenue Acounts
+        config_parameter='prod_qnt_cost_tracing.clearance_out_ids',
         # config_parameter='sale.allowed_account_ids'  # store in ir.config_parameter
     ) 
-    last_closing_year=fields.Date(string="Last Closing Date")
+    last_closing_year=fields.Date(string="Last Closing Date",
+                                  config_parameter='prod_qnt_cost_tracing.last_closing_year',
+                                 )
 
     @api.model
     def get_values(self):
