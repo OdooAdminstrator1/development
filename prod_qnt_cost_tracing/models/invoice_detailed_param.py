c4ba3b34cf6d9dc3e07e5f3b1504587dc767fe8c
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-04-28 17:10:24 +0300

    Update invoice_detailed_param.py

--- prod_qnt_cost_tracing/models/invoice_detailed_param.py
+++ prod_qnt_cost_tracing/models/invoice_detailed_param.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api, Command, _
 from datetime import datetime, timedelta
 
 class InvoiceDetailParam(models.Model):
@@ -13,38 +13,67 @@ class InvoiceDetailParam(models.Model):
     revenue_account = fields.Many2many('account.account'
                    ,'revenue_param_account_account_rel'
                    ,'param_id', 'account_id',string= 'Rev Account')
+
+    def prepare_for_query(self, distinct_revenus, distinct_costs):
+        """Create or Update the parameter record"""
+        existing = self.search([], limit=1)
+        vals = {
+            'todate': fields.Datetime.now(),
+            'cost_account': [Command.set(distinct_costs)],
+            'revenue_account': [Command.set(distinct_revenus)],
+        }
+        
+        if not existing:
+            return self.create(vals)
+        
+        existing.write(vals)
+        return existing
+
+    def prepare_for_datequery(self, fromdate, todate):
+        """Update existing or create new date parameters"""
+        existing = self.search([], limit=1)
+        vals = {
+            'fromdate': fromdate,
+            'todate': todate,
+        }
+        
+        if not existing:
+            return self.create(vals)
+        
+        existing.write(vals)
+        return existing
     
 
 
-    def prepare_for_query(self,distinct_revenus,distinct_costs):
-        """Create default record if none exists"""
-        if not self.search_count([]):
-            # Set dates (example: current month)
-            today = datetime.now()
-            return self.create({
-                'todate': today,
-                'cost_account': [(6, 0, distinct_costs)],
-                'revenue_account': [(6, 0, distinct_revenus)],
-            })
-        else:
-            today = datetime.now()
-            return self.write({
-                'todate': today,
-                'cost_account': [(6, 0, distinct_costs)],
-                'revenue_account': [(6, 0, distinct_revenus )],
-            })
+    # def prepare_for_query(self,distinct_revenus,distinct_costs):
+    #     """Create default record if none exists"""
+    #     if not self.search_count([]):
+    #         # Set dates (example: current month)
+    #         today = datetime.now()
+    #         return self.create({
+    #             'todate': today,
+    #             'cost_account': [(6, 0, distinct_costs)],
+    #             'revenue_account': [(6, 0, distinct_revenus)],
+    #         })
+    #     else:
+    #         today = datetime.now()
+    #         return self.write({
+    #             'todate': today,
+    #             'cost_account': [(6, 0, distinct_costs)],
+    #             'revenue_account': [(6, 0, distinct_revenus )],
+    #         })
     
-    def prepare_for_datequery(self,fromdate,todate):
-        """Create default record if none exists"""
-        if not self.search_count([]):
-            # Set dates (example: current month)
-            return self.create({
-                'fromdate': fromdate,
-                'todate': todate,
-            })
-        else:
-            return self.write({
-                'fromdate': fromdate,
-                'todate': todate,
-            })
+    # def prepare_for_datequery(self,fromdate,todate):
+    #     """Create default record if none exists"""
+    #     if not self.search_count([]):
+    #         # Set dates (example: current month)
+    #         return self.create({
+    #             'fromdate': fromdate,
+    #             'todate': todate,
+    #         })
+    #     else:
+    #         return self.write({
+    #             'fromdate': fromdate,
+    #             'todate': todate,
+    #         })
     
