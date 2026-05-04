e5b95266110b794fe3650afdd60c53231b768d9b
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-05-04 08:13:07 +0300

    Update product_trace.py

--- prod_qnt_cost_tracing/models/product_trace.py
+++ prod_qnt_cost_tracing/models/product_trace.py
@@ -516,7 +516,7 @@ and vl.account_move_id is not null;
             new_args += combined_domain  # extend, not append
 
         
-        res= super(ProductTrace, self)._search(domain= args, offset=offset, limit=limit, order=order, access_rights_uid=access_rights_uid)
+        res= super(ProductTrace, self)._search(domain= new_args, offset=offset, limit=limit, order=order, access_rights_uid=access_rights_uid)
         return res
 
     def getNormalTrace(self):
