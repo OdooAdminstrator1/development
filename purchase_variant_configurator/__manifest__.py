a459f91451353b874f2dfe3bc536a2ff057dafff
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-05-07 19:24:14 +0300

    Update __manifest__.py

--- purchase_variant_configurator/__manifest__.py
+++ purchase_variant_configurator/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Purchase - Product variants",
     "summary": "Product variants in purchase management",
-    "version": "17.0.1.0.1",
+    "version": "18.0.0.0",
     "license": "AGPL-3",
     "depends": ["purchase", "product_variant_configurator"],
     "author": "AvanzOSC, "
