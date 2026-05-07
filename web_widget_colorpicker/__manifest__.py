1a8faa47fc78dee4cf9848fa8bc13f52b5d82d1f
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-05-07 19:22:11 +0300

    Update __manifest__.py

--- web_widget_colorpicker/__manifest__.py
+++ web_widget_colorpicker/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": """Added Color Picker for From""",
     "category": "web",
     "images": ['static/description/icon.png'],
-    "version": "17.0.25.07.24",
+    "version": "18.0.0.0",
     "description": """
 
             For Form View - added = widget="colorpicker"
