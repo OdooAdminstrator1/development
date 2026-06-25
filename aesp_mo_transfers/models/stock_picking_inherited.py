4873a41b246b96bcab076554543e3a7dc8c2f23f
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-06-25 13:38:14 +0300

    Delete aesp_mo_transfers directory

--- aesp_mo_transfers/models/stock_picking_inherited.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-from odoo.exceptions import UserError
-
-
-class StockPickingInheritedMoTrans(models.Model):
-    _inherit = 'stock.picking'
-
-    related_mo_id = fields.Many2one('mrp.production')
