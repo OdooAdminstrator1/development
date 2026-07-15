f1b565dc96d33d815533678e6b6bc2de07ebc5ae
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-07-15 15:06:16 +0300

    Add files via upload

---
+++ product_power_current/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Product Power & Current",
+    "version": "17.0.1.0.0",
+    "category": "Inventory/Product",
+    "summary": "Adds Power and Current fields to product variants",
+    "author": "Your Company",
+    "license": "LGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_product_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
