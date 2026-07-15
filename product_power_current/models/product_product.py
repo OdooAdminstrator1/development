7b5609969a6a9fab6a404ac54b11a8ef630771e0
OdooAdminstrator1 <108800653+OdooAdminstrator1@users.noreply.github.com>
2026-07-15 20:47:20 +0300

    Update product_product.py

--- product_power_current/models/product_product.py
+++ product_power_current/models/product_product.py
@@ -4,10 +4,8 @@ from odoo import fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    power = fields.Float(
-        string="Power"
-    )
-
-    current = fields.Float(
-        string="Current"
-    )
+    power = fields.Float(string="Power")
+    min_voltage = fields.Float(string="Min Voltage")
+    max_voltage = fields.Float(string="Max Voltage")
+    mincurrent = fields.Float(string="Min Current")
+    maxcurrent = fields.Float(string="Max Current")
